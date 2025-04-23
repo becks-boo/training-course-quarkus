deb39eb8ba8b2a2ca507d091f90c683ad4189a78
Igor Stein <igy@MacBook-Pro-von-Igor.fritz.box>
2025-04-23 13:20:44 +0200

    first commit

---
+++ src/test/java/io/stein/OpenApiTests.java
@@ -0,0 +1,18 @@
+package io.stein;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Test;
+
+@QuarkusTest
+class OpenApiTests {
+
+    @Test
+    void whenGetOpenApi_thenReturn200() {
+        RestAssured.given()
+                .when()
+                .get("/openapi.yml")
+                .then()
+                .statusCode(200);
+    }
+}
